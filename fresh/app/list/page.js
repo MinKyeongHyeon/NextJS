b8ed7383b5955be350efe03d8d8fd4507080e746
MinKyeongHyeon <28599@naver.com>
2025-10-20 17:45:08 +0900

    study/ 리액트 복습 / state로 값 관리하는 방법 복습

--- fresh/app/list/page.js
+++ fresh/app/list/page.js
@@ -1,8 +1,12 @@
+"use client";
 import Link from "next/link";
 import Image from "next/image";
+import { useState } from "react";
 
 function List() {
   let product = ["Tomatoes", "Pasta", "Coconut"];
+  let [amount, setAmount] = useState([0, 0, 0]);
+
   return (
     <div>
       <h2>이 페이지는 라우팅된 페이지입니다.</h2>
@@ -12,9 +16,35 @@ function List() {
       <p>map으로 아이템도 나열해 보았습니다.</p>
       {product.map((v, i) => {
         return (
-          <div key={i} className="bg-gray-50 hover:bg-gray-300">
+          <div
+            key={i}
+            className="bg-gray-50 hover:bg-gray-300 flex flex-col items-center"
+          >
             <Image src={`/food${i}.png`} alt={v} width={120} height={120} />
             <h4>{v} 40$</h4>
+            <div className="flex gap-4">
+              <button
+                type="button"
+                onClick={() => {
+                  let copy = [...amount];
+                  copy[i]--;
+                  setAmount(copy);
+                }}
+              >
+                -
+              </button>
+              <p>{amount[i]}</p>
+              <button
+                type="button"
+                onClick={() => {
+                  let add = [...amount];
+                  add[i]++;
+                  setAmount(add);
+                }}
+              >
+                +
+              </button>
+            </div>
           </div>
         );
       })}
